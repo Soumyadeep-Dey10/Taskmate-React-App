c84aa56f137ef4a75900f7f73b5a699833630d5b
Soumyadeep <deepsoumya442@gmail.com>
2024-03-30 11:38:52 +0530

    Files are commited

---
+++ src/components/ShowTask.jsx
@@ -0,0 +1,36 @@
+export const ShowTask = ({taskList, setTaskList, task, setTask}) => {
+
+    const handleEdit = (id) => {
+        const seletedTask = taskList.find(todo => todo.id === id);
+        setTask(seletedTask);
+    }
+
+    const handleDelete = (id) => {
+        const updatedTasklist = taskList.filter(todo => todo.id !== id);
+        setTaskList(updatedTasklist);
+    }
+
+  return (
+    <section className="showTask">
+        <div className="head">
+            <div>
+                <span className="title">Your Tasks</span>
+                <span className="count">{taskList.length}</span>
+            </div>
+            <button onClick={() => setTaskList([])} className="clearAll">Clear All</button>
+        </div>
+        <ul>
+            { taskList.map((todo) => (
+                <li key={todo.id}>
+                    <p>
+                        <span className="name">{todo.name}</span>
+                        <span className="time">{todo.time}</span>
+                    </p>
+                    <i onClick={() => handleEdit(todo.id)} className="bi bi-pencil-square"></i>
+                    <i onClick={() => handleDelete(todo.id)} className="bi bi-trash"></i>
+                </li>
+            )) }            
+        </ul>
+    </section>
+  )
+}
